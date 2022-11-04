d25be18a12910a5258fc30859a7a95746af6d4b3
Trey Ramm <p.f.ramm@gmail.com>
2022-11-04 17:59:57 +0100

    updated lidar settings

--- Assets/Scripts/Settings/GameSettings.cs
+++ Assets/Scripts/Settings/GameSettings.cs
@@ -18,7 +18,7 @@ namespace Rover.Settings
         public const float GPS_COORD_Y_MIN = 19;
         public const float GPS_COORD_Y_MAX = 23;
         public const float TERRAIN_MAX = 1000;
-        public const float LIDAR_SCAN_RANGE = 20f;
+        public const float LIDAR_SCAN_RANGE = 50f;
     }
 }
 
