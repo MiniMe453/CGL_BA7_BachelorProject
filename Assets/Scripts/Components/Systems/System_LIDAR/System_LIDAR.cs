ed3ff5bef20294564a5a39bb735869cd0ceca76f
Trey Ramm <p.f.ramm@gmail.com>
2022-11-04 16:27:34 +0100

    implemented lidar system

--- Assets/Scripts/Components/Systems/System_LIDAR/System_LIDAR.cs
+++ Assets/Scripts/Components/Systems/System_LIDAR/System_LIDAR.cs
@@ -13,10 +13,16 @@ public class System_LIDAR : MonoBehaviour
     public int numOfLidarPoints;
     public Transform parentGameObject;
     public GameObject lidarPointPrefab;
+    public static event System.Action EOnObjectEnterRange;
+    public static event System.Action EOnObjectLeaveRange;
+    private Collider[] m_hitColliders = new Collider[] {};
+    private bool m_OnLeaveEventFired = false;
     void Awake()
     {
         m_LidarButton = new ArduinoInput(InputType.Digital, 43, 9, "LIDAR Button");
         m_LidarButton.EOnButtonPressed += OnButtonPressed;
+
+        Timer.Register(0.25f, () => CheckLidarArea(), isLooped: true);
     }
 
     void OnButtonPressed(int pin)
@@ -24,11 +30,16 @@ public class System_LIDAR : MonoBehaviour
         if(System_MTR.RoverVelocity > 0.1)
         {
             UIManager.ShowMessageBox("STOP THE ROVER", Color.red, 2f);  
+            return;
+        }
+        else if (m_hitColliders.Length == 0)
+        {
+            UIManager.ShowMessageBox("NO OBJECTS IN RANGE", Color.red, 2f);
         }
         else
         {
             OperatingSystem.SetUserControl(false);
-            UIManager.ShowMessageBox("TRANSMITTING...", Color.green, 2f);
+            UIManager.ShowMessageBox("SCANNING...", Color.green, 2f);
             Timer.Register(2f, () => SpawnLidarPoints());
         }
     }
@@ -36,9 +47,8 @@ public class System_LIDAR : MonoBehaviour
     void SpawnLidarPoints()
     {
         OperatingSystem.SetUserControl(true);
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, GameSettings.LIDAR_SCAN_RANGE);
 
-        foreach(Collider collider in hitColliders)
+        foreach(Collider collider in m_hitColliders)
         {
             Debug.Log(collider.gameObject.name);
             collider.gameObject.layer = LayerMask.NameToLayer("NavCameraViewMode");
@@ -48,5 +58,23 @@ public class System_LIDAR : MonoBehaviour
                 collider.gameObject.transform.GetChild(i).gameObject.layer = LayerMask.NameToLayer("NavCameraViewMode");
             }
         }
+    }
+
+    void CheckLidarArea()
+    {
+        m_hitColliders = Physics.OverlapSphere(transform.position, GameSettings.LIDAR_SCAN_RANGE, LayerMask.NameToLayer("NavCameraDetectionLayer"));
+
+        if(m_hitColliders.Length == 0)
+        {
+            if(!m_OnLeaveEventFired)
+            {
+                EOnObjectLeaveRange?.Invoke();
+                m_OnLeaveEventFired = true;
+            }
+            return;
+        }
+            
+        m_OnLeaveEventFired = false;
+        EOnObjectEnterRange?.Invoke();
     }
 }
